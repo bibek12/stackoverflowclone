f4ca24b870c54dc4505b555a0b361f756d9d53d4
bibek12 <sbibek100@gmail.com>
2019-03-01 12:39:17 +0545

    stackover floder without syntax highliter

---
+++ app/Http/Controllers/VoteAnswerController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Answer;
+use Illuminate\Http\Request;
+
+class VoteAnswerController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function __invoke(Answer $answer)
+    {
+        $vote = (int) request()->vote;
+
+       $votesCount= auth()->user()->voteAnswer($answer, $vote);
+        if (request()->expectsJson()) {
+            return response()->json([
+                'message' => 'Your answer has been updated',
+                'votesCount'=>$votesCount
+            ]);
+        }
+        return back();
+    }
+}
---
+++ resources/views/questions/show.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <question :question="{{$question}}"></question>
+    <answers :question="{{ $question}}"></answers>
+    
+ 
+</div>
+@endsection
